e134ae96345d1b3b61cfd2c554b90284e4fc6d4f
Ahmed98 <ahmeddigger1998@gmail.com>
2025-11-12 08:07:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# electronics_store_e_commerce_with_admin_panel
+
+coding along with Shivam Gupta's tutorial
+
---
+++ android/app/src/main/kotlin/com/example/electronics_store_e_commerce_with_admin_panel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.electronics_store_e_commerce_with_admin_panel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
